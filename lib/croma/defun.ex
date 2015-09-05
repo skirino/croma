2343cf0c893ca0c7a14bb4c43ef8dffd1ba725d8
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2015-09-05 19:50:04 +0900

    Refactoring: Unite code for argument conversion

--- lib/croma/defun.ex
+++ lib/croma/defun.ex
@@ -77,15 +77,17 @@ defmodule Croma.Defun do
   end
 
   defp defun_impl(def_or_defp, {fname, env, args0}, ret_type, type_params, block) do
-    args = if is_atom(args0), do: [], else: args0 # handle function definition without parameter list: it looks like a variable
-    fun = {fname, env, args}
-    spec = typespec(fun, ret_type, type_params)
-    bodyless = bodyless_function(def_or_defp, fun)
-    fundef = function_definition(def_or_defp, fun, block)
+    args = case args0 do
+      fcontext when is_atom(fcontext) -> []                      # function definition without parameter list
+      _                               -> List.first(args0) || [] # 1 argument: name-type keyword list
+    end
+    spec = typespec(fname, env, args, ret_type, type_params)
+    bodyless = bodyless_function(def_or_defp, fname, env, args)
+    fundef = function_definition(def_or_defp, fname, env, args, block)
     {:__block__, [], [spec, bodyless, fundef]}
   end
 
-  defp typespec({fname, env, args}, ret_type, type_params) do
+  defp typespec(fname, env, args, ret_type, type_params) do
     func_with_return_type = {:::, [], [{fname, [], arg_types(args)}, ret_type]}
     spec_expr = case type_params do
       [] -> func_with_return_type
@@ -97,24 +99,21 @@ defmodule Croma.Defun do
   end
 
   defp arg_types(args) do
-    (List.first(args) || [])
-    |> Keyword.values
-    |> Enum.map(fn
+    Keyword.values(args) |> Enum.map(fn
       {:\\, _, [type, _default]} -> type
       type                       -> type
     end)
   end
 
-  defp bodyless_function(def_or_defp, {fname, env, args}) do
-    arg_exprs = (List.first(args) || [])
-    |> Enum.map(fn
+  defp bodyless_function(def_or_defp, fname, env, args) do
+    arg_exprs = Enum.map(args, fn
       {name, {:\\, _, [_type, default]}} -> {:\\, [], [{name, [], Elixir}, default]}
       {name, _type}                      -> {name, [], Elixir}
     end)
     {def_or_defp, env, [{fname, env, arg_exprs}]}
   end
 
-  defp function_definition(def_or_defp, {fname, env, args}, block) do
+  defp function_definition(def_or_defp, fname, env, args, block) do
     defs = case block do
       {:__block__, _, multiple_defs} -> multiple_defs
       single_def                     -> List.wrap(single_def)
@@ -124,7 +123,7 @@ defmodule Croma.Defun do
       {:__block__, env, clause_defs}
     else
       # Ugly workaround for variable context issues with nested macro invocations: Overwrite context of every variables
-      arg_names = (List.first(args) || []) |> Keyword.keys |> Enum.map(&Macro.var(&1, Croma))
+      arg_names = Keyword.keys(args) |> Enum.map(&Macro.var(&1, Croma))
       block_with_modified_context = Macro.prewalk(block, fn
         {name, meta, context} when is_atom(context) -> {name, meta, Croma}
         t -> t
