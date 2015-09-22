986bbed9525699b15412351b61fb167daedbb387
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2015-09-22 18:42:07 +0900

    Add dialyze

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule Croma.Mixfile do
       {:excheck, "~> 0.2", only: :test},
       {:triq, github: "krestenkrab/triq", only: :test},
       {:coverex, "~> 1.4", only: :test},
+      {:dialyze, "~> 0.2", only: :dev},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.7", only: :dev},
       {:inch_ex, only: :docs},
