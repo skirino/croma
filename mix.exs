3fb3231d6ae7bb056ad0c26b38e1c8c9b8943255
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2015-11-24 05:05:39 +0900

    Update tools

--- mix.exs
+++ mix.exs
@@ -23,13 +23,13 @@ defmodule Croma.Mixfile do
 
   defp deps do
     [
-      {:excheck, "~> 0.2", only: :test},
+      {:excheck, "~> 0.3", only: :test},
       {:triq, github: "krestenkrab/triq", only: :test},
       {:coverex, "~> 1.4", only: :test},
       {:dialyze, "~> 0.2", only: :dev},
       {:earmark, "~> 0.1", only: :dev},
-      {:ex_doc, "~> 0.7", only: :dev},
-      {:inch_ex, only: :docs},
+      {:ex_doc, "~> 0.11", only: :dev},
+      {:inch_ex, "~> 0.4", only: :docs},
     ]
   end
 
