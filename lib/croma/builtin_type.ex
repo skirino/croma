478c60d522652b19ac98a591bc0d918d87a7a7cb
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2015-11-24 04:29:14 +0900

    Fix incorrect type annotations for builtin type wrappers

--- lib/croma/builtin_type.ex
+++ lib/croma/builtin_type.ex
@@ -4,35 +4,35 @@ defmodule Croma.BuiltinType do
   @moduledoc false
 
   @type_infos [
-    {Croma.Atom     , :atom      , :is_atom     },
-    {Croma.Boolean  , :boolean   , :is_boolean  },
-    {Croma.Float    , :float     , :is_float    },
-    {Croma.Integer  , :integer   , :is_integer  },
-    {Croma.String   , :"String.t", :is_binary   },
-    {Croma.BitString, :bitstring , :is_bitstring},
-    {Croma.Function , :function  , :is_function },
-    {Croma.Pid      , :pid       , :is_pid      },
-    {Croma.Port     , :port      , :is_port     },
-    {Croma.Reference, :reference , :is_reference},
-    {Croma.Tuple    , :tuple     , :is_tuple    },
-    {Croma.List     , :list      , :is_list     },
-    {Croma.Map      , :map       , :is_map      },
+    {Croma.Atom     , :is_atom     , "atom"     , quote do: atom     },
+    {Croma.Boolean  , :is_boolean  , "boolean"  , quote do: boolean  },
+    {Croma.Float    , :is_float    , "float"    , quote do: float    },
+    {Croma.Integer  , :is_integer  , "integer"  , quote do: integer  },
+    {Croma.String   , :is_binary   , "String.t" , quote do: String.t },
+    {Croma.BitString, :is_bitstring, "bitstring", quote do: bitstring},
+    {Croma.Function , :is_function , "function" , quote do: function },
+    {Croma.Pid      , :is_pid      , "pid"      , quote do: pid      },
+    {Croma.Port     , :is_port     , "port"     , quote do: port     },
+    {Croma.Reference, :is_reference, "reference", quote do: reference},
+    {Croma.Tuple    , :is_tuple    , "tuple"    , quote do: tuple    },
+    {Croma.List     , :is_list     , "list"     , quote do: list     },
+    {Croma.Map      , :is_map      , "map"      , quote do: map      },
   ]
 
   def type_infos, do: @type_infos
   def all do
-    Enum.map(@type_infos, fn {m, _, _} -> m end)
+    Enum.map(@type_infos, fn {m, _, _, _} -> m end)
   end
 end
 
-Croma.BuiltinType.type_infos |> Enum.each fn {mod, builtin_type, pred} ->
+Croma.BuiltinType.type_infos |> Enum.each fn {mod, pred, type_name, type_expr} ->
   defmodule mod do
     @moduledoc """
-    Module that represents the Elixir's built-in #{builtin_type} type.
-    Intended to be used with other parts of croma to express #{builtin_type} variables.
+    Module that represents the Elixir's built-in #{type_name} type.
+    Intended to be used with other parts of croma to express #{type_name} variables.
     """
 
-    @type t :: unquote(builtin_type)
+    @type t :: unquote(type_expr)
 
     @doc """
     Simply checks the argument's type using `#{pred}/1` and returns a `Croma.Result`.
