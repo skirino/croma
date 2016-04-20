6db7fd255da7e39daf2a78b5d1aaa63fe7dccb5f
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2016-04-20 13:50:18 +0900

    v0.4.3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Croma.Mixfile do
   def project do
     [
       app:             :croma,
-      version:         "0.4.2",
+      version:         "0.4.3",
       elixir:          "~> 1.2",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
