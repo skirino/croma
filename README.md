3743dd085ec0a5f6705b48c24162523100a702fd
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2019-02-24 22:20:25 +0900

    Test: Migrate from coverex to excoveralls

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Elixir macro utilities to make type-based programming easier.
 [![Hex.pm](http://img.shields.io/hexpm/v/croma.svg)](https://hex.pm/packages/croma)
 [![Hex.pm](http://img.shields.io/hexpm/dt/croma.svg)](https://hex.pm/packages/croma)
 [![Build Status](https://travis-ci.org/skirino/croma.svg)](https://travis-ci.org/skirino/croma)
-[![Coverage Status](https://coveralls.io/repos/skirino/croma/badge.png?branch=master)](https://coveralls.io/r/skirino/croma?branch=master)
+[![Coverage Status](https://coveralls.io/repos/github/skirino/croma/badge.svg?branch=master)](https://coveralls.io/github/skirino/croma?branch=master)
 
 ## Usage
 
--- mix.exs
+++ mix.exs
@@ -5,17 +5,18 @@ defmodule Croma.Mixfile do
 
   def project() do
     [
-      app:             :croma,
-      version:         "0.10.1",
-      elixir:          "~> 1.6",
-      build_embedded:  Mix.env() == :prod,
-      start_permanent: Mix.env() == :prod,
-      deps:            deps(),
-      description:     "Elixir macro utilities to make type-based programming easier",
-      package:         package(),
-      source_url:      @github_url,
-      homepage_url:    @github_url,
-      test_coverage:   [tool: Coverex.Task, coveralls: true],
+      app:               :croma,
+      version:           "0.10.1",
+      elixir:            "~> 1.6",
+      build_embedded:    Mix.env() == :prod,
+      start_permanent:   Mix.env() == :prod,
+      deps:              deps(),
+      description:       "Elixir macro utilities to make type-based programming easier",
+      package:           package(),
+      source_url:        @github_url,
+      homepage_url:      @github_url,
+      test_coverage:     [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
     ]
   end
 
@@ -25,11 +26,11 @@ defmodule Croma.Mixfile do
 
   defp deps() do
     [
-      {:stream_data, "~> 0.4"   , only: :test},
-      {:coverex    , "~> 1.4"   , only: :test},
-      {:dialyxir   , "~> 0.5"   , only: :dev},
-      {:earmark    , "~> 1.2"   , only: :dev},
-      {:ex_doc     , "~> 0.18.0", only: :dev},
+      {:dialyxir   , "~> 0.5"   , [only: :dev ]},
+      {:earmark    , "~> 1.2"   , [only: :dev ]},
+      {:ex_doc     , "~> 0.18.0", [only: :dev ]},
+      {:stream_data, "~> 0.4"   , [only: :test]},
+      {:excoveralls, "~> 0.10"  , [only: :test]},
     ]
   end
 
